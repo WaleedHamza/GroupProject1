50bd9fd0699c4345fb4378f203483be28c17f14c
thill2 <hill341539@gmail.com>
2018-04-10 21:13:21 -0400

    updated java

--- assets/javascript/operations.js
+++ assets/javascript/operations.js
@@ -22,40 +22,72 @@
 });
 //search buttoon functions//
 
+var address = "";
+var title = "";
+
 $("#searchBtn").click(function(){
   $("#searchDiv").show(300);
 });
 $("#goBtn").click(function(){
-  var address = $("#address").val().trim();
-  var title = $("#title").val().trim();
-  if(title === "" && address === ""){
-    alert("Please tell us where should we look ?! ")
-    return false
-  }else{
-    $("#searchDiv").hide(300);
+  address = $("#address").val().trim().replace(/\s/g, "+");
+  title = $("#title").val().trim().replace(/\s/g, "+");
   
-  }
-});
+console.log("placesLocaion: " + address);
+  // if(title === "" && address === ""){
+  //   alert("Please tell us where should we look ?! ")
+  //   return false
+  // }else{
+  //   $("#searchDiv").hide(300);
+  //}
 
+  // ajax to pull lat and lng for initial map
+  $.ajax({
+    url: "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" + address + "&search=" + title + "&key=AIzaSyDYL3PAu6qXoGN_ii_Ld_cfYCQ7FY-AMl0",
+    method: "GET"
+  }).then(function(spot) {
+    console.log(spot.results["0"].geometry.location.lat);
+    console.log(spot.results["0"].geometry.location.lng);
+    initMap(spot.results["0"].geometry.location.lat, spot.results["0"].geometry.location.lng)
+  })
+
+  
+  // ajax to pull jobs based on search location
+  var zipQueryURL = "https://api.ziprecruiter.com/jobs/v1?search=Perl%20Job&location="+ address +"&radius_miles=25&days_ago=&jobs_per_page=10&page=1&api_key=gjetj6yzdta73384442bezn9sp8tfwbe";
+  console.log(zipQueryURL);
+
+  $.ajax({
+    url: zipQueryURL,
+    method: "GET"
+  }).then(function(response) {
+    for (var i = 0; i<response.jobs.length; i++) {
+    searchCompanies.push(response.jobs[i].hiring_company.name)
+    //console.log(searchCompanies)
+    locations.push(response.jobs[i].location)
+    // console.log(locations)
+    };
+    console.log(searchCompanies);
+    console.log(searchCompanies[1]);
+});
 
   // google maps functions
   var map;
   var infowindow;
-  function initMap() {
-    var durham = {lng: -78.9025080566, lat: 35.9883427133 };
+  function initMap(intLat, intLng) {
+    var durham = {lng: intLng, lat: intLat };
+    console.log(durham);
   
     map = new google.maps.Map(document.getElementById('map'), {
       center: durham,
       zoom: 10
     });
-  
+ 
     infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
       location: durham,
       radius: 35000,
       // change this to get different companies to display on map using searchCompanies array
-      name: ['IBM']
+      name: ["RiVi Consulting"]
     }, callback);
   }
   
@@ -68,6 +100,7 @@ $("#goBtn").click(function(){
     }
   }
   
+  // creating pins on map
   function createMarker(place) {
     var placeLoc = place.geometry.location;
     var marker = new google.maps.Marker({
@@ -85,19 +118,5 @@ $("#goBtn").click(function(){
 var searchCompanies = [];
 var locations = [];
 // take user input and place location here
-var searchPlace = "Durham, NC"
-var zipQueryURL = "https://api.ziprecruiter.com/jobs/v1?search=Perl%20Job&location="+ searchPlace +"&radius_miles=25&days_ago=&jobs_per_page=10&page=1&api_key=gjetj6yzdta73384442bezn9sp8tfwbe";
-
-  $.ajax({
-    url: zipQueryURL,
-    method: "GET"
-  }).then(function(response) {
-    for (var i = 0; i<response.jobs.length; i++) {
-    searchCompanies.push(response.jobs[i].hiring_company.name)
-    console.log(searchCompanies)
-    locations.push(response.jobs[i].location)
-    // console.log(locations)
-    };
+// var searchPlace = "durham";
   });
-
-
